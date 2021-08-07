62cb9f6ee6684b9647aba21de4ac9fa88dfb3199
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-07 20:40:11 -0300

    style: adding global style vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,37 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: "mdi",
+    },
+    theme: {
+        options: {
+            customProperties: true,
+        },
+        customVariables: ["../styles/variables.scss"],
+        treeShake: true,
+        dark: false,
+        themes: {
+            light: {
+                primary: "#6C63FF",
+                secondary: "#6C39FF",
+                accent: "#82B1FF",
+                error: "#F91A42",
+                info: "#2196F3",
+                success: "#15B843",
+                warning: "#FFB82F",
+            },
+            dark: {
+                primary: {
+                    base: "#2BED90",
+                },
+                secondary: "#6C39FF", 
+                accent: "#82B1FF",
+                error: "#F91A42",
+                info: "#2196F3",
+                success: "#15B843",
+                warning: "#FFB82F",
+            },
+        },
+    },
 });
