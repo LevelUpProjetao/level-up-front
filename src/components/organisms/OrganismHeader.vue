d6fa79e9c101cfa593554709bc98ab360ee88c70
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-07 20:38:46 -0300

    feat: adding OrganismHeader.vue

---
+++ src/components/organisms/OrganismHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#fff"
+      dark
+    >
+      <v-img
+        alt="LevelUp Logo"
+        class="shrink ml-5"
+        contain
+        src="../../assets/logo.png"
+        transition="scale-transition"
+        width="204"
+      />
+      
+
+      <v-spacer />
+
+      <div class="centerElements mr-5 my-10">
+        <MoleculeUserPhoto />
+      
+        <h3 class="ml-3">
+          Jorge Antonio
+        </h3>
+      </div>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+import MoleculeUserPhoto from '../molecules/MoleculeUserPhoto.vue'
+export default {
+  name: 'App',
+  components:{
+    MoleculeUserPhoto
+  },
+
+  data: () => ({
+    //
+  }),
+  methods:{
+    goToLogin(){
+      router.push("/login");
+    }
+  }
+};
+</script>
+<style scoped>
+.centerElements{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
