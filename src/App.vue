ac6a5478c85b11fb6fa425af13fba3250171b705
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-07 20:39:44 -0300

    feat: registering header on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer />
-
-      <v-btn
-        target="_blank"
-        text
-        @click="goToLogin"
-      >
-        <span class="mr-2">Ir para pagina de login</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <OrganismHeader />
     <v-main>
       <router-view />
     </v-main>
@@ -45,8 +9,12 @@
 
 <script>
 import router from "./router";
-export default {
+import OrganismHeader from './components/organisms/OrganismHeader.vue';
+  export default {
   name: 'App',
+  components:{
+    OrganismHeader
+  },
 
   data: () => ({
     //
@@ -58,3 +26,10 @@ export default {
   }
 };
 </script>
+<style scoped>
+.centerElements{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
