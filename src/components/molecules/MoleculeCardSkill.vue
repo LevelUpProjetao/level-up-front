b4a78f34153ebb47e6559eab270a81c87ba2dbd9
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-08 20:53:14 -0300

    feat: adding MoleculeCardSkill

---
+++ src/components/molecules/MoleculeCardSkill.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-title>
+      <h3 class="title_skill">
+        Oratória
+      </h3>
+    </v-card-title>
+    <v-card-subtitle>
+      <h4 class="proguess_skill">
+        Em proguesso
+      </h4>
+    </v-card-subtitle>
+    <v-card-text>
+      <p class="text_skill">
+        Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+</script>
+<style scoped>
+.title_skill{
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 32px;
+}
+.proguess_skill{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 22px;
+    color:red
+}
+.text_skill{
+    
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 22px;
+}
+</style>
