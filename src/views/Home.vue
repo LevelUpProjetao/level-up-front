67b9a45cdfc294c633f0fe56c73f237856cb775c
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-08 20:55:44 -0300

    feat: finish home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,46 @@
 <template>
-  <hello-world />
+  <v-row justify="space-between">
+    <v-col cols="12">
+      <OrganismMySkills />
+    </v-col>
+    <v-col cols="5">
+      <OrganismProguess />
+      <OrganismNewResource class="mt-5" />
+    </v-col>
+    <v-col cols="7">
+      <OrganismSkills />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import OrganismMySkills from "../components/organisms/OrganismMySkills.vue"
+import OrganismSkills from "../components/organisms/OrganismSkills.vue"
+import OrganismProguess from "../components/organisms/OrganismProguess.vue"
+import OrganismNewResource from "../components/organisms/OrganismNewResource.vue"
+export default {
+  name: 'Home',
+  components:{
+    OrganismMySkills,
+    OrganismSkills,
+    OrganismProguess,
+    OrganismNewResource
+  },
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+  data: () => ({
+    //
+  }),
+  methods:{
+    goToLogin(){
+      router.push("/login");
+    }
   }
+};
 </script>
+<style scoped>
+.centerElements{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
