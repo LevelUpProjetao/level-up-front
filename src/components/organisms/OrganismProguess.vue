ca9345cb7ed896a8f72e0f70f6a98886872f820b
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-08-29 16:39:27 -0300

    feat: finishing organismProguess

--- src/components/organisms/OrganismProguess.vue
+++ src/components/organisms/OrganismProguess.vue
@@ -19,12 +19,12 @@
           :rotate="-90"
           :size="100"
           :width="15"
-          :value="value"
+          :value="getValue"
           color="#27AE60"
           style="width:130px; height:130px"
           class="ml-3"
         >
-          {{ value }}%
+          {{ getValue }}%
         </v-progress-circular>
       </v-col>
       <v-col
@@ -32,10 +32,10 @@
         md="6"
         class="text-start"
       >
-        <h3>2/8</h3>
+        <h3>{{ finilized_skills }}/{{ total_skills }}</h3>
         <span>Skill adquiridas</span>
         <h3 class="mt-2">
-          13
+          {{ resourceCounts }}
         </h3>
         <span>Recursos compartilhados</span>
       </v-col>
@@ -44,19 +44,36 @@
 </template>
 
 <script>
+import api from "../../api/axios"
 
 export default {
   components:{
     
   },
-
   data: () => ({
-    value:12.5
+    value:12.5,
+    finilized_skills: 0,
+    total_skills: 0,
+    resourceCounts:0
   }),
-  methods:{
-    goToLogin(){
-      router.push("/login");
+  computed:{
+    getValue(){
+      if(this.resourceCounts == 0){
+        return 0
+      }else{
+        return (this.finilized_skills/this.resourceCounts)*100
+      }
     }
+  },
+  
+  async created(){
+    console.log('user: ');
+    console.log(this.$store.state.user);
+    const skillsCounts = (await api.get(`/users/${this.$store.state.user.email}/skills_count`)).data
+    this.finilized_skills = skillsCounts.finilized_skills
+    this.total_skills = skillsCounts.total_skills
+    const resourceCounts = (await api.get(`/users/${this.$store.state.user.email}/resources_count`)).data
+    console.log(resourceCounts);
   }
 };
 </script>
